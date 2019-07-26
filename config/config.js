2804fcaf3be4bc45e432d38b58f096d6414dd426
André Ferreira <andreferreira@MacBook-Pro-de-Andre.local>
2019-07-26 20:56:09 -0300

    conexão sql lite

---
+++ config/config.js
@@ -0,0 +1,13 @@
+export default {
+    database: 'books',
+    username: '',
+    password: '',
+    params: {
+        dialect: 'sqlite',
+        storage: 'books.sqlite',
+        operatorsAliases: false,
+        define: {
+            underscore: true
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import app from './app';
 
-app.listen('3000', ()=>{
-    console.log('app runing on port 3000');
+app.listen(app.get('port'), ()=>{
+    console.log(`app runing on port 3000 ${app.get('port')}`);
 });
